c84657c7564c6ec8a8247f1434059a663251d034
Joshua Tomazin <jtomazin@bmbp.lan>
2020-12-13 14:47:49 -0500

    add package.json and grid class

---
+++ src/grid.js
@@ -0,0 +1,35 @@
+'use strict'
+
+_ = require('lodash')
+
+let Grid = () => {
+    grid = {}
+    
+    return {
+        get: (x, y) => {
+            if (grid[x] == null) {
+                return null
+            } else {
+                return grid[x][y]
+            }
+        }
+        
+        set: (x, y, val) => {
+            if (grid[x] == null) {
+                grid[x] = {}
+            }
+            grid[x][y] = val
+            return grid
+        }
+
+        getNeighbors: (x, y) => {
+            return _.flatten(
+                // [x-1, x, x+1].map((x) => [y-1, y, y-2]grid[x])
+            )
+        }
+    }
+}
+
+
+
+module.exports = Grid

e5c4d0c4b7bb66d844b7ca0b7e1369ecdeeb7973
Joshua Tomazin <jtomazin@bmbp.lan>
2020-12-13 16:35:03 -0500

    beef up tile

--- src/server/tile.js
+++ src/server/tile.js
@@ -3,7 +3,7 @@
 _ = require('lodash')
 
 // Different types of tile
-TYPES = {
+const TYPES = {
   FIELD: 0
   CITY: 1
   ROAD: 2
@@ -11,7 +11,8 @@ TYPES = {
   // mission?
 }
 
-SIDES = {
+// also number of clockwise rotations
+const SIDES = {
   TOP: 0
   RIGHT: 1
   BOTTOM: 2
@@ -30,14 +31,35 @@ SIDES = {
    C C C        C C C        C F C
    
    As a basic rule, edges matter for piece placement, and corners matter for connectivity
+
+   `geography` - 3x3 array of TYPES
+   `rotation` - SIDES
  */
 
-Tile = () => {
+const SIDE_TO_GRID_LOOKUP = {
+  [TOP]: [0, 1]
+  [RIGHT]: [1, 2]
+  [BOTTOM]: [2, 1]
+  [LEFT]: [1, 0]
+}
 
-  return {
-    getEdges: () => []
-          
+let Tile = (geography, rotation) => {
+
+  let getSide = (side) => {
+    rotatedSide = (side + rotation) % 4 
+    [r, c] = SIDE_TO_GRID_LOOKUP[rotatedSide]
+    return geography[r][c]
   }
+  
+  return {
+    getSide,
+    getSides: () => _.fromPairs(
+      _.values(SIDES).map((side) => [side, getSide(side)])
+    )
+    
+  }
+  
+}
 }
 
 Module.exports = {Tile, TYPES, SIDES}
